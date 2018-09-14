3f887ca454c59a2757b3b2ab5fc4d54273926bf9
evanwatterud <cod.videos@hotmail.com>
2018-09-14 14:18:03 -0400

    more routing boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import Navigation from './Navigation'
+import LandingPage from './Landing'
+import SignUpPage from './SignUp'
+import SignInPage from './SignIn'
+import DashboardPage from './Dashboard'
+
+import * as routes from '../constants/routes'
 import '../css/App.css'
 
 const App = () => {
   return (
     <Router>
-      <Navigation />
+      <div>
+        <Navigation />
+        <hr />
+        <Route
+          exact
+          path={routes.LANDING}
+          component={LandingPage}
+        />
+        <Route
+          exact
+          path={routes.SIGN_UP}
+          component={SignUpPage}
+        />
+        <Route
+          exact
+          path={routes.SIGN_IN}
+          component={SignInPage}
+        />
+        <Route
+          exact
+          path={routes.DASHBOARD}
+          component={DashboardPage}
+        />
+      </div>
     </Router>
   )
 }
