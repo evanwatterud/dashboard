f8ebe78df6271e2d5e0f3e838aed8f17e1321b95
evanwatterud <cod.videos@hotmail.com>
2018-09-15 13:41:01 -0400

    added sign out functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import SignOutButton from './SignOut'
 import * as routes from '../constants/routes'
 
-const Navigation = () => {
+const NavigationAuth = () => {
+  return (
+    <ul>
+      <li><Link to={routes.LANDING}>Landing</Link></li>
+      <li><Link to={routes.DASHBOARD}>Dashboard</Link></li>
+      <li><SignOutButton /></li>
+    </ul>
+  )
+}
+
+const NavigationNonAuth = () => {
+  return (
+    <ul>
+      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+      <li><Link to={routes.LANDING}>Landing</Link></li>
+    </ul>
+  )
+}
+
+const Navigation = ({ authUser }) => {
   return (
     <div>
-      <ul>
-        <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-        <li><Link to={routes.SIGN_UP}>Sign Up</Link></li>
-        <li><Link to={routes.LANDING}>Landing</Link></li>
-        <li><Link to={routes.DASHBOARD}>Dashboard</Link></li>
-      </ul>
+      { authUser ? <NavigationAuth /> : <NavigationNonAuth /> }
     </div>
   )
+}
+
+Navigation.propTypes = {
+  authUser: PropTypes.shape({}).isRequired
 }
 
 export default Navigation
