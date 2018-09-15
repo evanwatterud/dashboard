39a19f314287c1702dbaa00ae688a531faf57c3b
evanwatterud <cod.videos@hotmail.com>
2018-09-15 13:16:57 -0400

    added sign in functionality

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const SignIn = () => {
+import { SignUpLink } from './SignUp'
+import { auth } from '../firebase'
+import * as routes from '../constants/routes'
+
+const SignInPage = ({ history }) => {
   return (
     <div>
       <h1>Sign In Page</h1>
+      <SignInForm history={history} />
+      <SignUpLink />
     </div>
   )
 }
 
-export default SignIn
+SignInPage.propTypes = {
+  history: PropTypes.shape({}).isRequired
+}
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
+}
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { ...INITIAL_STATE }
+  }
+
+  onSubmit = (event) => {
+    const { email, password } = this.state
+
+    const { history } = this.props
+
+    auth.doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE })
+        history.push(routes.DASHBOARD)
+      })
+      .catch((err) => {
+        this.setState({ error: err })
+      })
+
+    event.preventDefault()
+  }
+
+  handleChange = (key, value) => { this.setState({ [key]: value }) }
+
+  render() {
+    const { email, password, error } = this.state
+
+    const isInvalid = password === '' || email === ''
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={email}
+          onChange={(event) => { this.handleChange('email', event.target.value) }}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={password}
+          onChange={(event) => { this.handleChange('password', event.target.value) }}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    )
+  }
+}
+
+SignInForm.propTypes = {
+  history: PropTypes.shape({}).isRequired
+}
+
+export default withRouter(SignInPage)
+
+export {
+  SignInForm
+}
